c774e0f36a82bbcbbfd7a9cac662737d635ef27c
dhruvja <dhruv@iamsizzling.com>
2023-05-27 13:24:16 +0530

    added methods for creating market and bets

--- programs/parimutuel-sports/src/lib.rs
+++ programs/parimutuel-sports/src/lib.rs
@@ -1,46 +1,189 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount};
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
+use switchboard_v2::AggregatorAccountData;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BvnkwA2K3R41Hex1M56ck5BQktu7RBKLaEFqitiLNDHU");
 
 const MARKET_STATE_SEED: &'static [u8] = b"market_state";
 const MARKET_WALLET_SEED: &'static [u8] = b"market_wallet";
+const USER_STATE_SEED: &'static [u8] = b"user_state";
+
+const PLATFORM_FEES: u8 = 1;
 #[program]
 pub mod parimutuel_sports {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, game_id: String) -> Result<()> {
+    pub fn create_market(
+        ctx: Context<CreateMarket>,
+        game_id: String,
+        feed: Pubkey,
+        outcomes: Vec<String>,
+        expiry_time: u64,
+        creator_fees: u8,
+    ) -> Result<()> {
+        let market_state = &mut ctx.accounts.market_state;
+
+        market_state.game_id = game_id;
+        market_state.feed = feed;
+        market_state.expiry_time = expiry_time;
+        market_state.creator_fees = creator_fees;
+        market_state.creator = ctx.accounts.creator.key();
+        market_state.token_mint = ctx.accounts.token_mint.key();
+
+        let mut total_outcomes: Vec<Outcome> = Vec::new();
+        for outcome in outcomes {
+            let new_outcome = Outcome {
+                total_bet_amount: 0,
+                total_bets: 0,
+                name: outcome,
+            };
+            total_outcomes.push(new_outcome);
+        }
+        market_state.outcomes = total_outcomes;
+        Ok(())
+    }
+
+    pub fn bet(
+        ctx: Context<Bet>,
+        game_id: String,
+        state_bump: u8,
+        outcome: String,
+        bet_amount: u64,
+    ) -> Result<()> {
+        let market_state = &mut ctx.accounts.market_state;
+        let user_bet_state = &mut ctx.accounts.user_bet_state;
+
+        let expiry_time = market_state.expiry_time;
+        let current_time = Clock::get()?.unix_timestamp as u64;
+
+        if expiry_time < current_time {
+            return Err(error!(ErrorCodes::MarketExpired));
+        }
+
+        let token_mint = market_state.token_mint;
+        let user_token_mint = ctx.accounts.token_mint.key();
+        if token_mint != user_token_mint {
+            return Err(error!(ErrorCodes::MarketExpired));
+        }
+
+        // Transfer the bet amount to the pool
+
+        let bump_vector = state_bump.to_le_bytes();
+        let inner = vec![
+            MARKET_STATE_SEED,
+            game_id.as_bytes()[..18].as_ref(),
+            game_id.as_bytes()[18..].as_ref(),
+            bump_vector.as_ref(),
+        ];
+        let outer = vec![inner.as_slice()];
+
+        // Below is the actual instruction that we are going to send to the Token program.
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.bettor_token_account.to_account_info(),
+            to: ctx.accounts.market_wallet.to_account_info(),
+            authority: ctx.accounts.bettor.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            outer.as_slice(), //signer PDA
+        );
+
+        anchor_spl::token::transfer(cpi_ctx, bet_amount)?;
+
+        msg!("transfer happened");
+
+        user_bet_state.outcome = outcome.clone();
+        user_bet_state.bet = bet_amount;
+
+        let outcomes = &mut market_state.outcomes;
+        // let outcome_index = outcomes.iter().find(|&x| x.name == outcome).ok_or_else(err)
+        let mut outcome_index = usize::MAX;
+        for i in 0..outcomes.len() {
+            if &outcomes[i].name == &outcome {
+                outcome_index = i;
+                break;
+            }
+        }
+
+        outcomes[outcome_index].total_bet_amount += bet_amount;
+        outcomes[outcome_index].total_bets += 1;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 #[instruction(game_id: String)]
-pub struct Initialize<'info> {
+pub struct CreateMarket<'info> {
     #[account(mut)]
     pub creator: Signer<'info>,
     #[account(init, payer = creator, seeds = [MARKET_STATE_SEED, game_id.as_bytes()[..18].as_ref(), game_id.as_bytes()[18..].as_ref()],bump, space = 1000)]
-    pub market: Account<'info, Market>,
-    pub another_wallet: Account<'info, TokenAccount>,
+    pub market_state: Account<'info, Market>,
     pub token_mint: Account<'info, Mint>,
-    #[account(init, payer = creator, seeds = [MARKET_WALLET_SEED, game_id.as_bytes()[..18].as_ref(), game_id.as_bytes()[18..].as_ref()], bump, token::mint = token_mint, token::authority = market)]
+    #[account(init, payer = creator, seeds = [MARKET_WALLET_SEED, game_id.as_bytes()[..18].as_ref(), game_id.as_bytes()[18..].as_ref()], bump, token::mint = token_mint, token::authority = market_state)]
     pub market_wallet: Account<'info, TokenAccount>,
+    // pub switchboard_aggregator: AccountLoader<'info, AggregatorAccountData>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub rent: Sysvar<'info, Rent>,
 }
 
+#[derive(Accounts)]
+#[instruction(game_id: String)]
+pub struct Bet<'info> {
+    #[account(mut)]
+    pub bettor: Signer<'info>,
+    #[account(
+        mut,
+        constraint=bettor_token_account.owner == bettor.key(),
+        constraint=bettor_token_account.mint == token_mint.key()
+    )]
+    pub bettor_token_account: Account<'info, TokenAccount>,
+    #[account(mut, seeds = [MARKET_STATE_SEED, game_id.as_bytes()[..18].as_ref(), game_id.as_bytes()[18..].as_ref()],bump)]
+    pub market_state: Account<'info, Market>,
+    #[account(init, payer = bettor, seeds = [USER_STATE_SEED, game_id.as_bytes()[..18].as_ref(), game_id.as_bytes()[18..].as_ref(), bettor.key().as_ref()],bump, space = 48 + 8 + 8)]
+    pub user_bet_state: Account<'info, UserBet>,
+    pub token_mint: Account<'info, Mint>,
+    #[account(mut, seeds = [MARKET_WALLET_SEED, game_id.as_bytes()[..18].as_ref(), game_id.as_bytes()[18..].as_ref()], bump)]
+    pub market_wallet: Account<'info, TokenAccount>,
+    // pub switchboard_aggregator: AccountLoader<'info, AggregatorAccountData>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
 
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize, PartialEq)]
+pub struct Outcome {
+    pub total_bets: u64,       // 8
+    pub total_bet_amount: u64, // 32*10
+    pub name: String,          // 40
+}
 
 #[account]
 pub struct Market {
     pub game_id: String,
-    pub win_odds: u8,
-    pub loss_odds: u8,
-    pub game_label: String,
-    pub end_time: u64,
-    pub buffer_time: u64,
-    pub token_mint: Pubkey,
-    pub position_count: u8,
+    pub expiry_time: u64,
+    pub feed: Pubkey,
     pub creator: Pubkey,
+    pub creator_fees: u8,
+    pub outcomes: Vec<Outcome>,
+    pub token_mint: Pubkey,
+}
+
+#[account]
+pub struct UserBet {
+    pub outcome: String, // 40
+    pub bet: u64,        // 8
+}
+
+#[error_code]
+#[derive(Eq, PartialEq)]
+pub enum ErrorCodes {
+    #[msg("Switchboard feed has not been updated in 5 minutes")]
+    StaleFeed,
+    #[msg("The betting is closed")]
+    MarketExpired,
+    #[msg("The market token mint doesnt match the user token mint")]
+    TokenMintMismatch,
 }
--- tests/parimutuel-sports.ts
+++ tests/parimutuel-sports.ts
@@ -1,16 +1,311 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { ParimutuelSports } from "../target/types/parimutuel_sports";
+import { bs58 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+const assert = require("assert");
+import * as spl from "@solana/spl-token";
+import { v4 as uuidv4 } from "uuid";
+import { PublicKey } from "@solana/web3.js";
 
 describe("parimutuel-sports", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.ParimutuelSports as Program<ParimutuelSports>;
+  console.log(provider.connection.rpcEndpoint);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace
+    .ParimutuelSports as Program<ParimutuelSports>;
+
+  // TODO: Add docs of what each of whese keys represent.
+  let alice: anchor.web3.Keypair; // Just another Keypair
+  let bob: anchor.web3.Keypair; // Just another Keypair
+  let cas: anchor.web3.Keypair; // stakeholder who would stake on the application
+  let dan: anchor.web3.Keypair; // Just another keypair
+  let admin: anchor.web3.Keypair; // This is the authority which is responsible for creating job, application and changing state of application
+
+  let USDCMint: anchor.web3.PublicKey; // token which would be staked
+  let casTokenAccount: any; // cas token account
+  let aliceTokenAccount: any; // alice Token account
+
+  let initialMintAmount = 100000000;
+  const stakeAmount = 4000;
+  const maxAmountPerApplication = 10000;
+
+  if (provider.connection.rpcEndpoint == "http://localhost:8899") {
+    alice = anchor.web3.Keypair.generate(); // HR
+    bob = anchor.web3.Keypair.generate(); // Applicant
+    cas = anchor.web3.Keypair.generate(); // Stakeholder
+    dan = anchor.web3.Keypair.generate(); // Stakeholder
+    admin = anchor.web3.Keypair.generate(); // Admin
+
+    it("Funds all users", async () => {
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(alice.publicKey, 10000000000),
+        "confirmed"
+      );
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(bob.publicKey, 10000000000),
+        "confirmed"
+      );
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(cas.publicKey, 10000000000),
+        "confirmed"
+      );
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(dan.publicKey, 10000000000),
+        "confirmed"
+      );
+
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(admin.publicKey, 10000000000),
+        "confirmed"
+      );
+
+      const aliceUserBalance = await provider.connection.getBalance(
+        alice.publicKey
+      );
+      const bobUserBalance = await provider.connection.getBalance(
+        bob.publicKey
+      );
+      const casUserBalance = await provider.connection.getBalance(
+        cas.publicKey
+      );
+      const danUserBalance = await provider.connection.getBalance(
+        dan.publicKey
+      );
+      const adminUserBalance = await provider.connection.getBalance(
+        admin.publicKey
+      );
+
+      assert.strictEqual(10000000000, aliceUserBalance);
+      assert.strictEqual(10000000000, bobUserBalance);
+      assert.strictEqual(10000000000, casUserBalance);
+      assert.strictEqual(10000000000, danUserBalance);
+      assert.strictEqual(10000000000, adminUserBalance);
+    });
+
+    it("create USDC mint and mint some tokens to stakeholders", async () => {
+      USDCMint = await spl.createMint(
+        provider.connection,
+        admin,
+        admin.publicKey,
+        null,
+        6
+      );
+
+      casTokenAccount = await spl.createAccount(
+        provider.connection,
+        cas,
+        USDCMint,
+        cas.publicKey
+      );
+
+      aliceTokenAccount = await spl.createAccount(
+        provider.connection,
+        alice,
+        USDCMint,
+        alice.publicKey
+      );
+
+      await spl.mintTo(
+        provider.connection,
+        cas,
+        USDCMint,
+        casTokenAccount,
+        admin.publicKey,
+        initialMintAmount,
+        [admin]
+      );
+
+      await spl.mintTo(
+        provider.connection,
+        alice,
+        USDCMint,
+        aliceTokenAccount,
+        admin.publicKey,
+        initialMintAmount,
+        [admin]
+      );
+
+      let casTokenAccountUpdated = await spl.getAccount(
+        provider.connection,
+        casTokenAccount
+      );
+      let aliceTokenAccountUpdated = await spl.getAccount(
+        provider.connection,
+        aliceTokenAccount
+      );
+
+      assert.equal(initialMintAmount, casTokenAccountUpdated.amount);
+      assert.equal(initialMintAmount, aliceTokenAccountUpdated.amount);
+    });
+  } else {
+    // These are the private keys of accounts which i have created and have deposited some SOL in it.
+    // Since we cannot airdrop much SOL on devnet (fails most of the time), i have previously airdropped some SOL so that these accounts
+    // can be used for testing on devnet.
+    // We can have them in another file and import them. But these are only for testing and has 0 balance on mainnet.
+    const alicePrivate =
+      "472ZS33Lftn7wdM31QauCkmpgFKFvgBRg6Z6NGtA6JgeRi1NfeZFRNvNi3b3sh5jvrQWrgiTimr8giVs9oq4UM5g";
+    const casPrivate =
+      "4CpgQ2g3KojCEpLwUDVjzFNWoMbvUqqQodHMPjN6B71mRy7dCuwWxCW8F9zjUrxsYDJyDpu1cbiERc8bkFR41USt";
+    const adminPrivate =
+      "2HKjYz8yfQxxhRS5f17FRCx9kDp7ATF5R4esLnKA4VaUsMA5zquP5XkQmvv9J5ZUD6wAjD4iBPYXDzQDNZmQ1eki";
+
+    alice = anchor.web3.Keypair.fromSecretKey(
+      new Uint8Array(bs58.decode(alicePrivate))
+    );
+    cas = anchor.web3.Keypair.generate();
+    admin = anchor.web3.Keypair.fromSecretKey(
+      new Uint8Array(bs58.decode(adminPrivate))
+    );
+
+    USDCMint = new anchor.web3.PublicKey(
+      "CAb5AhUMS4EbKp1rEoNJqXGy94Abha4Tg4FrHz7zZDZ3"
+    );
+
+    it("Get the associated token account and mint tokens", async () => {
+      // await provider.connection.confirmTransaction(
+      //   await provider.connection.requestAirdrop(alice.publicKey, 100000000),
+      //   "confirmed"
+      // );
+
+      const TempAliceTokenAccount = await spl.getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        alice,
+        USDCMint,
+        alice.publicKey,
+        false
+      );
+
+      aliceTokenAccount = TempAliceTokenAccount.address;
+
+      const _aliceTokenAccountBefore = await spl.getAccount(
+        provider.connection,
+        aliceTokenAccount 
+      );
+
+      await spl.mintTo(
+        provider.connection,
+        alice,
+        USDCMint,
+        aliceTokenAccount,
+        admin.publicKey,
+        initialMintAmount,
+        [admin]
+      );
+
+      const _aliceTokenAccountAfter = await spl.getAccount(
+        provider.connection,
+        aliceTokenAccount
+      );
+
+      assert.equal(
+        initialMintAmount,
+        _aliceTokenAccountAfter.amount - _aliceTokenAccountBefore.amount
+      );
+    });
+  }
+
+  const gameId = uuidv4();
+  const outcomes = ["HOME", "AWAY"];
+  const feedKey = new PublicKey("GZkZoR3tRcEWfqkvXk2A6XQHpS2etN8rkD3NeP5VaRVe");
+
+  it("create market", async () => {
+    const [marketStatePDA, marketStateBump] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from("market_state"),
+          Buffer.from(gameId.substring(0, 18)),
+          Buffer.from(gameId.substring(18, 36)),
+        ],
+        program.programId
+      );
+
+    const [marketPoolPDA, marketPoolBump] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from("market_wallet"),
+          Buffer.from(gameId.substring(0, 18)),
+          Buffer.from(gameId.substring(18, 36)),
+        ],
+        program.programId
+      );
+
+    const expiryTime = Date.now() + 1000000;
+    const tx = await program.methods
+      .createMarket(gameId, feedKey, outcomes, new anchor.BN(expiryTime), 10)
+      .accounts({
+        creator: alice.publicKey,
+        marketState: marketStatePDA,
+        tokenMint: USDCMint,
+        marketWallet: marketPoolPDA,
+        // switchboardAggregator: feedKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      })
+      .signers([alice])
+      .rpc();
+
+    console.log("THis is tx for create market", tx);
+  });
+
+  it("bet", async () => {
+    const [marketStatePDA, marketStateBump] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from("market_state"),
+          Buffer.from(gameId.substring(0, 18)),
+          Buffer.from(gameId.substring(18, 36)),
+        ],
+        program.programId
+      );
+
+    const [marketPoolPDA, marketPoolBump] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from("market_wallet"),
+          Buffer.from(gameId.substring(0, 18)),
+          Buffer.from(gameId.substring(18, 36)),
+        ],
+        program.programId
+      );
+    
+    const [userStatePDA, userStateBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("user_state"),
+        Buffer.from(gameId.substring(0, 18)),
+        Buffer.from(gameId.substring(18, 36)),
+        alice.publicKey.toBuffer()
+      ],
+      program.programId
+    )
+
+    const outcome = "HOME";
+    const betAmount = 10000;
+    try {
+      const tx = await program.methods
+      .bet(gameId, marketStateBump, outcome, new anchor.BN(betAmount))
+      .accounts({
+        bettor: alice.publicKey,
+        bettorTokenAccount: aliceTokenAccount,
+        marketState: marketStatePDA,
+        userBetState: userStatePDA,
+        tokenMint: USDCMint,
+        marketWallet: marketPoolPDA,
+        // switchboardAggregator: feedKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      })
+      .signers([alice])
+      .rpc();
+      console.log("THis is tx for bet", tx);
+    } catch (error) {
+      console.log("this is error", error);
+    }
+    
+
   });
 });
